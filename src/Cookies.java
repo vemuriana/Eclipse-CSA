b7340f6c62af2675b655dc6fcf9e32856d793189
1047224 <1047224@486E303557.lwsd.k12wa.org>
2024-09-19 09:55:47 -0700

    Making cookies

--- src/Cookies.java
+++ src/Cookies.java
@@ -1,20 +1,35 @@
 public class Cookies {
 
 	public static void main(String[] args) {
+		//Making the batter
+		makeBatter();
+		
+		//Bake cookies
+		bakeCookies();
+		bakeCookies();
+		
+		//Decorate cookies
+		decorateCookies();
+		
+		
+	}
+	
+	public static void makeBatter() {
 		System.out.println("Mix the dry ingredients.");
 		System.out.println("Cream the butter and sugar.");
 		System.out.println("Beat in the eggs.");
 		System.out.println("Stir in the dry ingredients.");
+	}
+
+	public static void bakeCookies() {
 		System.out.println("Set the oven temperature.");
 		System.out.println("Set the timer.");
 		System.out.println("Place a batch of cookies into the oven.");
 		System.out.println("Allow the cookies to bake.");
-		System.out.println("Set the oven temperature.");
-		System.out.println("Set the timer.");
-		System.out.println("Place a batch of cookies into the oven.");
-		System.out.println("Allow the cookies to bake.");
+	}
+	
+	public static void decorateCookies() {
 		System.out.println("Mix ingredients for frosting.");
 		System.out.println("Spread frosting and sprinkles.");
 	}
-
 }
